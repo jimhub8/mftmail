4f8118618bcf651c6fb962c5bd013b37e7f02560
jimkiarie8@gmail.com <jimmy@localhost.localdomain>
2021-01-13 14:33:11 +0300

    commit

--- app/Http/Controllers/SendMailController.php
+++ app/Http/Controllers/SendMailController.php
@@ -14,9 +14,13 @@ class SendMailController extends Controller
         $this->Validate($request, [
             'full_name' => 'required',
             'phone' => 'required',
-            'address' => 'required',
+            'address' => 'required'
         ]);
+
+        $message = 'Thank you for placing an order. A customer service agent will get in touch with you soon. For any inquiry please email us on sales@dala.market';
+
+
         Mail::send(new SendMail($request->all()));
-        return redirect()->back()->with('message', 'Thank you for placing the order');
+        return redirect()->back()->with('message', $message);
     }
 }
