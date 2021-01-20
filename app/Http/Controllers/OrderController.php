04bc107df51c26c9b18f526d8bfd137c4f0463a5
jimkiarie8@gmail.com <jimmy@localhost.localdomain>
2021-01-20 09:51:15 +0300

    commit

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -38,12 +38,12 @@ class OrderController extends Controller
         $data = $request->all();
         $order = Order::latest()->first('order_no');
         if ($order) {
-            $order_arr = explode('_', $order->order_no);
+            $order_arr = explode('-', $order->order_no);
             $order_no = (int)$order_arr[1] + 1;
         } else {
             $order_no = 1005;
         }
-        $data['order_no'] = 'DET_' . $order_no;
+        $data['order_no'] = 'DET-' . $order_no;
 
         $message = 'Thank you for placing an order. A customer service agent will get in touch with you soon. For any inquiry please email us on sales@dala.market';
 
